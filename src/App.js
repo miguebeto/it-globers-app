6256fc64ae9507ff7a2e32177d68d99b152872d3
Miguel Blanco <mindmusic7@gmail.com>
2022-01-21 09:10:04 -0500

    added basic configuration and css styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React from "react";
+import Header from "./header/Header";
+
 import './App.css';
 
-function App() {
+const vuelos = [
+  {id: '1', name: 'Vivair'},
+  {id: '2', name: 'Avianca'}
+]
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <h3 style={{ padding: "40px" }}>Hola Bienvenido, sabemos que quieres en un { vuelos[1].name }</h3>
     </div>
   );
 }
 
-export default App;
---
+++ src/header/Header.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { ReactComponent as CloseMenu } from "../assets/x.svg";
+import { ReactComponent as MenuIcon } from "../assets/menu.svg";
+import { ReactComponent as Logo } from "../assets/logo.svg";
+import "./header.css";
+
+const Header = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+  return (
+    <BrowserRouter>
+    <div className="header"> 
+      <div className="logo-nav">
+        <div className="logo-container">
+          <a href="#">
+            <Logo className="logo" />
+          </a>
+        </div>
+        <ul className={click ? "nav-options active" : "nav-options"}>
+          <li className="option" onClick={closeMobileMenu}>
+            <a href="#">ABOUT</a>
+          </li>
+          <li className="option" onClick={closeMobileMenu}>
+            <a href="#">CONTACT</a>
+          </li>
+        </ul>
+      </div>
+      <div className="mobile-menu" onClick={handleClick}>
+        {click ? (
+          <CloseMenu className="menu-icon" />
+        ) : (
+          <MenuIcon className="menu-icon" />
+        )}
+      </div>
+    </div>
+    </BrowserRouter>
+  );
+};
+
+export default Header;
