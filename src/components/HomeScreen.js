1de894980227796afacae9f0b5a921049d350b1c
Miguel Blanco <mindmusic7@gmail.com>
2022-01-22 15:07:33 -0500

    added airline Vivacolombia

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -7,11 +7,14 @@ export const HomeScreen = () => {
 
     let params = useParams();
 
-    return (
-        <div>
-            <h1>Home</h1>
-            <h3 style={{ padding: "40px" }}>Hola, Bienvenido, sabemos que quieres viajar en un {params.nameAirline === 'it-globers-app' ? null : params.nameAirline}</h3>
-            <Form />
-        </div>
-    )
+  return (
+    <div>
+      <h1>Home</h1>
+      <h3 style={{ padding: "40px" }}>
+        Hola, Bienvenido, sabemos que quieres viajar en un{" "}
+        {params.invoiceId === "it-globers-app" ? null : params.invoiceId}
+      </h3>
+      <Form />
+    </div>
+  );
 };
