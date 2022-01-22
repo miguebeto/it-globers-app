7dd19325c2a3d0f84882351027df7190483e2154
Miguel Blanco <mindmusic7@gmail.com>
2022-01-22 14:29:22 -0500

    added optimization and conditional rendering

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -5,6 +5,7 @@ import { Form } from './Form';
 
 export const HomeScreen = () => {
 
+<<<<<<< HEAD
     let params = useParams();
 
     return (
@@ -14,4 +15,16 @@ export const HomeScreen = () => {
             <Form />
         </div>
     )
+=======
+  return (
+    <div>
+      <h1>Home</h1>
+      <h3 style={{ padding: "40px" }}>
+        Hola, Bienvenido, sabemos que quieres viajar en un{" "}
+        {params.invoiceId === "it-globers-app" ? null : params.nameAirline}
+      </h3>
+      <Form />
+    </div>
+  );
+>>>>>>> parent of 0350c0e (Revert "added optimizations and conditional rendering")
 };
