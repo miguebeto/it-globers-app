1b499847eac2c0656e786a95e8cfe45d82f18b49
Miguel Blanco <mindmusic7@gmail.com>
2022-01-22 16:38:59 -0500

    Update HomeScreen.js

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -11,7 +11,7 @@ export const HomeScreen = () => {
       <h1>Home</h1>
       <h3 style={{ padding: "40px" }}>
         Hola, Bienvenido, sabemos que quieres viajar en un
-        {params.nameAirline === "it-globers-app" ? null : params.nameAirline}
+         {params.nameAirline === "it-globers-app" ? null : params.nameAirline}
       </h3>
       <Form />
     </div>
